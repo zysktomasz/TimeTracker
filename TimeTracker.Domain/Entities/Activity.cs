2941670bd0088a79a5c51830b8732b853cbda727
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-05 22:29:04 +0100

    add Activity entity

---
+++ TimeTracker.Domain/Entities/Activity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Domain.Entities
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public string Name { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime? TimeEnd { get; set; }
+        public int? TimeTotal { get; set; }
+    }
+}

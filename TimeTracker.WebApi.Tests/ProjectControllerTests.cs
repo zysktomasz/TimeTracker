c3e1468bf2c2c3e558c2e8e402f8f3fccf4206fd
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-10 13:11:23 +0100

    install packages - Moq, Microsoft.AspNetCore.All

---
+++ TimeTracker.WebApi.Tests/ProjectControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TimeTracker.WebApi.Tests
+{
+    public class ProjectControllerTests
+    {
+        // arrange
+        
+        // act
+
+        // assert
+    }
+}

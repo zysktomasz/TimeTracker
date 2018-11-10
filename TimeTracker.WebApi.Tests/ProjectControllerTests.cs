cf1b2d887ea25b1042d5e59cf1d827a7eb161e8e
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-10 13:38:32 +0100

    Add first ProjectController's test

--- TimeTracker.WebApi.Tests/ProjectControllerTests.cs
+++ TimeTracker.WebApi.Tests/ProjectControllerTests.cs
@@ -1,14 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using TimeTracker.Services.Interfaces;
+using TimeTracker.WebApi.Controllers;
 using Xunit;
 
 namespace TimeTracker.WebApi.Tests
 {
     public class ProjectControllerTests
     {
-        // arrange
-        
-        // act
+        [Fact]
+        public void GetProjectById_ExistingIdPassed_ReturnsOkResult()
+        {
+            // arrange
+            var serviceMock = new Mock<IProjectService>();
+            serviceMock
+                .Setup(s => s.GetProjectById(5))
+                .Returns(new Services.DTO.Project.ProjectDto { ProjectID = 5, Name = "Whatever" });
+            var controller = new ProjectController(serviceMock.Object);
 
-        // assert
+            // act
+            var result = controller.GetProjectById(5);
+
+            // assert
+            Assert.IsType<OkObjectResult>(result);
+        }
     }
 }

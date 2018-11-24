f370275b59919174a22fb37e655fc470991c0b62
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-24 16:20:50 +0100

    updated ActivityStartReturnDto to incude ActivityID and ProjectName

--- TimeTracker.Services/DTO/Activity/ActivityStartReturnDto.cs
+++ TimeTracker.Services/DTO/Activity/ActivityStartReturnDto.cs
@@ -6,8 +6,10 @@ namespace TimeTracker.Services.DTO.Activity
 {
     public class ActivityStartReturnDto
     {
+        public int ActivityID { get; set; }
         public string Name { get; set; }
         public DateTime? TimeStart { get; set; }
         public int? ProjectID { get; set; }
+        public string ProjectName { get; set; }
     }
 }

02b7fa8aca206b97d18c160c859fcf0f21e9767a
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-24 15:25:18 +0000

    Resolve "Aktywnosc - dodac automatycznie "TimeStart" jesli klient nie podał ręcznie"

--- TimeTracker.Services/DTO/Activity/ActivityStartReturnDto.cs
+++ TimeTracker.Services/DTO/Activity/ActivityStartReturnDto.cs
@@ -4,10 +4,12 @@ using System.Text;
 
 namespace TimeTracker.Services.DTO.Activity
 {
-    public class ActivityStartDto
+    public class ActivityStartReturnDto
     {
+        public int ActivityID { get; set; }
         public string Name { get; set; }
         public DateTime? TimeStart { get; set; }
         public int? ProjectID { get; set; }
+        public string ProjectName { get; set; }
     }
 }

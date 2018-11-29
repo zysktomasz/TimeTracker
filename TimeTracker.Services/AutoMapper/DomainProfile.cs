73e0005cfe44b958477a43563490caa762427d5b
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-11-29 12:43:00 +0100

    created DomainProfile class with initial mapping

---
+++ TimeTracker.Services/AutoMapper/DomainProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeTracker.Domain.Entities;
+using TimeTracker.Services.DTO.Activity;
+
+namespace TimeTracker.Services.AutoMapper
+{
+    public class DomainProfile : Profile
+    {
+        public DomainProfile()
+        {
+            CreateMap<Activity, ActivityDto>();
+        }
+    }
+}

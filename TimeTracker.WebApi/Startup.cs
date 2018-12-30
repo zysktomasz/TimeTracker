b85ce4e90162c3f0d47970762393f98c486b5b55
Tomasz Zysk <zysk.tomasz95@gmail.com>
2018-12-30 13:46:56 +0100

    configured support of jwt authentication

--- TimeTracker.WebApi/Startup.cs
+++ TimeTracker.WebApi/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TimeTracker.Domain.Identity;
 using TimeTracker.Persistance;
 using TimeTracker.Services.DTO.Activity;
@@ -37,6 +41,33 @@ namespace TimeTracker.WebApi
             // Add DbContext
             services.AddDbContext<TimeTrackerDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("TimeTrackerDatabase")));
+
+            // ===== Add Jwt Authentication ========
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                })
+                .AddJwtBearer(options =>
+                {
+                    //options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true, // validate the server that created this token
+                        ValidateAudience = true, // ensure that the recipient of the token is authorized to receive it 
+                        ValidateLifetime = true, // check that the token is not expired and that the signing key of the issuer is valid
+                        ValidateIssuerSigningKey = true, // verify that the key used to sign the incoming token is part of a list of trusted keys
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                        RequireSignedTokens = true
+                        //RequireExpirationTime = true,
+                        //ClockSkew = TimeSpan.FromMinutes(5)
+                    };
+                });
 
             // Add Identity
             // ===== Add Identity ========
@@ -70,6 +101,7 @@ namespace TimeTracker.WebApi
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
